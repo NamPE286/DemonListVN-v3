064bc5927181863fe3953746eb87079d76f44d59
NamPE286 <nambuihung654@gmail.com>
2024-04-16 19:42:16 +0700

    update

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Title from '$lib/components/Title.svelte';
+	import { user } from '$lib/client';
+</script>
+
+{#if $user.loggedIn && $user.data.isAdmin}
+	<Title value="Admin" />
+
+	<div class="wrapper">
+		<a href="/admin/ratingEstimator">Rating estimator</a>
+	</div>
+{/if}
+
+<style lang="scss">
+	a {
+		color: #7cb4f8;
+	}
+
+	.wrapper {
+		padding-inline: 75px;
+	}
+</style>
---
+++ src/routes/admin/ratingEstimator/+page.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import LevelCard from '$lib/components/levelCard.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import supabase from '$lib/client/supabase';
+	import { onMount } from 'svelte';
+	import { user } from '$lib/client';
+	import Title from '$lib/components/Title.svelte';
+	import Loading from '$lib/components/animation/loading.svelte';
+
+	let l = -1,
+		r = -1;
+
+	let lvStart: any = null,
+		lvEnd: any = null;
+
+	let level: any = null;
+
+	function mid() {
+		return Math.floor((l + r) / 2);
+	}
+
+	async function getLevelCount() {
+		const { data, error } = await supabase
+			.from('levels')
+			.select('id, dlTop')
+			.not('dlTop', 'is', null)
+			.order('dlTop', { ascending: false })
+			.limit(1)
+			.single();
+
+		return data?.dlTop;
+	}
+
+	async function fetchLevel(top: number) {
+		const { data, error } = await supabase
+			.from('levels')
+			.select('*')
+			.eq('dlTop', top)
+			.limit(1)
+			.single();
+
+		return data;
+	}
+
+	async function setEasier() {
+		l = mid();
+		level = null;
+		level = await fetchLevel(mid());
+
+		if (l == r - 1) {
+			fetchRatingRange();
+		}
+	}
+
+	async function setHarder() {
+		r = mid();
+		level = null;
+		level = await fetchLevel(mid());
+
+		if (l == r - 1) {
+			fetchRatingRange();
+		}
+	}
+
+	async function fetchRatingRange() {
+		lvStart = await fetchLevel(l);
+		lvEnd = await fetchLevel(r);
+	}
+
+	onMount(async () => {
+		l = 1;
+		r = await getLevelCount();
+		level = await fetchLevel(mid());
+	});
+</script>
+
+{#if $user.loggedIn && $user.data.isAdmin}
+	<Title value="Rating estimator" />
+
+	<div class="wrapper">
+		{#if l == r - 1}
+			{#if lvStart && lvEnd}
+				<p>Estimated rating range: {lvStart.rating} - {lvEnd.rating}</p>
+				<div class="finWrapper">
+					<LevelCard level={{ data: lvStart }} type="dl" />
+					<LevelCard level={{ data: lvEnd }} type="dl" />
+				</div>
+			{:else}
+				<Loading inverted />
+			{/if}
+		{:else if level}
+			<div class="lvWrapper">
+				<LevelCard level={{ data: level }} type="dl" />
+				<div class="btn">
+					<Button on:click={setEasier}>Easier</Button>
+					<span>or</span>
+					<Button on:click={setHarder}>Harder</Button>
+				</div>
+				<div>Placement range: {r} - {l}</div>
+			</div>
+		{:else}
+			<Loading inverted />
+		{/if}
+	</div>
+{/if}
+
+<style lang="scss">
+	.lvWrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 10px;
+	}
+
+	.finWrapper {
+		display: flex;
+		gap: 20px;
+		margin-top: 20px;
+	}
+
+	.wrapper {
+		padding: 80px;
+	}
+
+	.btn {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 10px;
+	}
+</style>
