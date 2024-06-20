220f6d3e1f0ed236f08c270d562b3f4561c4e43b
NamPE286 <nambuihung654@gmail.com>
2024-06-20 20:15:31 +0700

    update

--- src/routes/player/[uid]/+page.svelte
+++ src/routes/player/[uid]/+page.svelte
@@ -33,7 +33,7 @@
 
 {#if data.player.isBanned}
 	<div class="flex h-[50px] items-center justify-center bg-red-600">
-		This player has been banned.
+		This player is banned.
 	</div>
 {:else if data.player.isHidden}
 	<div class="flex h-[50px] items-center justify-center bg-yellow-600">This profile is hidden.</div>
