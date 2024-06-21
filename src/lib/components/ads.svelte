c9222d8df602e1ae5be2d8cd3979fbe8d7ecaaec
NamPE286 <nambuihung654@gmail.com>
2024-06-21 18:17:13 +0700

    update

--- src/lib/components/ads.svelte
+++ src/lib/components/ads.svelte
@@ -1,14 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
-	import { mediaQuery } from 'svelte-legos';
-
-	let isDesktop = true;
 
 	onMount(() => {
-		if (mediaQuery('(min-width: 1200px)')) {
-			isDesktop = true;
-		}
-
 		try {
 			window.addEventListener('load', () => {
 				// @ts-ignore
@@ -22,21 +15,22 @@
 </script>
 
 <div id="ad-container">
-	{#if isDesktop}
+	<div class="desktop">
 		<ins
 			class="adsbygoogle"
 			style="display:inline-block;width:728px;height:90px"
 			data-ad-client="ca-pub-4605218533506777"
 			data-ad-slot="7284584064"
 		></ins>
-	{:else}
+	</div>
+	<div class="mobile">
 		<ins
 			class="adsbygoogle"
 			style="display:inline-block;width:320px;height:100px"
 			data-ad-client="ca-pub-4605218533506777"
 			data-ad-slot="8384269781"
 		></ins>
-	{/if}
+	</div>
 </div>
 
 <style lang="scss">
@@ -46,5 +40,17 @@
 		justify-content: center;
 		margin-top: 20px;
 		margin-bottom: 20px;
+	}
+
+	@media screen and (max-width: 1200px) {
+		.mobile {
+			display: none;
+		}
+	}
+
+	@media screen and (min-width: 1200px) {
+		.desktop {
+			display: none;
+		}
 	}
 </style>
