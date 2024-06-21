529a8bc5c6ed91f337c8f3a4d97d622e3b6bd87e
NamPE286 <nambuihung654@gmail.com>
2024-06-21 13:36:49 +0700

    update

--- src/lib/components/ads.svelte
+++ src/lib/components/ads.svelte
@@ -2,24 +2,30 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		// @ts-ignore
-		(window.adsbygoogle = window.adsbygoogle || []).push({});
+		try {
+			let ins = document.createElement('ins');
+			ins.className = 'adsbygoogle';
+			ins.style.display = 'block';
+			ins.setAttribute('data-ad-client', 'ca-pub-4605218533506777');
+			ins.setAttribute('data-ad-slot', '7284584064');
+			ins.setAttribute('data-ad-format', 'auto');
+			ins.setAttribute('data-full-width-responsive', 'true');
+
+			//@ts-ignore
+			document.getElementById('ad-container').appendChild(ins);
+			//@ts-ignore
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		} catch (err) {
+			console.log('failed to load ads');
+			console.error(err);
+		}
 	});
 </script>
 
-<div class="ads-widget-container">
-	<ins
-		class="adsbygoogle"
-		style="display:block"
-		data-ad-client="ca-pub-4605218533506777"
-		data-ad-slot="7284584064"
-		data-ad-format="auto"
-		data-full-width-responsive="true"
-	></ins>
-</div>
+<div id="ad-container" />
 
 <style lang="scss">
-	.ads-widget-container {
+	#ad-container {
 		width: 100%;
 		display: flex;
 		justify-content: center;
