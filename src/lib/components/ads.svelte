65c1dd5aab1d65c49b81830684fd3e70d28ee905
NamPE286 <nambuihung654@gmail.com>
2024-06-21 19:32:08 +0700

    update

--- src/lib/components/ads.svelte
+++ src/lib/components/ads.svelte
@@ -14,42 +14,13 @@
 	});
 </script>
 
-<style lang="scss">
-	#ad-container {
-		width: fit-content;
-		margin-inline: auto;
-		margin-top: 20px;
-		margin-bottom: 20px;
-	}
-
-	@media screen and (min-width: 1200px) {
-		.mobile {
-			display: none;
-		}
-	}
-
-	@media screen and (max-width: 1200px) {
-		.desktop {
-			display: none;
-		}
-	}
-</style>
-
-<div id="ad-container">
-	<div class="desktop">
-		<ins
-			class="adsbygoogle"
-			style="display:inline-block;width:728px;height:90px"
-			data-ad-client="ca-pub-4605218533506777"
-			data-ad-slot="7284584064"
-		></ins>
-	</div>
-	<div class="mobile">
-		<ins
-			class="adsbygoogle"
-			style="display:inline-block;width:320px;height:100px"
-			data-ad-client="ca-pub-4605218533506777"
-			data-ad-slot="8384269781"
-		></ins>
-	</div>
+<div id="ad-container" class="w-fit bg-white mr-auto ml-auto">
+	<ins
+		class="adsbygoogle"
+		style="display:block"
+		data-ad-client="ca-pub-4605218533506777"
+		data-ad-slot="7284584064"
+		data-ad-format="auto"
+		data-full-width-responsive="true"
+	></ins>
 </div>
