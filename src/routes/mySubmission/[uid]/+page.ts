de45d4941f566e4831840bc9400375b58c4249f2
NamPE286 <nambuihung654@gmail.com>
2024-08-29 01:03:35 +0700

    update

--- src/routes/mySubmission/[uid]/+page.ts
+++ src/routes/mySubmission/[uid]/+page.ts
@@ -1,26 +1,7 @@
 export async function load({ params, fetch }) {
-    const query = new URLSearchParams({
-        end: '500',
-        isChecked: 'false',
-        sortBy: 'timestamp'
-    })
-    interface Result {
-        dl: any[],
-        fl: any[]
-    }
-
-    const res: Result = await (await fetch(`${import.meta.env.VITE_API_URL}/player/${params.uid}/records?${query.toString()}`)).json()
-    const data = res.dl.concat(res.fl)
-
-    data.sort((a, b) => {
-        if (a.timestamp < b.timestamp) {
-            return -1;
-        }
-
-        return 1;
-    })
+    const res: any[] = await (await fetch(`${import.meta.env.VITE_API_URL}/player/${params.uid}/submissions?end=500`)).json()
 
     return {
-        data: data
+        data: res
     }
 };
