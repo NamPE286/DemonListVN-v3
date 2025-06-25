dee36f44ff4e33db9c5acb7fe5969f34096ba598
NamPE286 <nambuihung654@gmail.com>
2025-06-25 14:43:03 +0700

    allow link once

--- src/lib/components/settingButton.svelte
+++ src/lib/components/settingButton.svelte
@@ -227,11 +227,7 @@
 			</Tabs.Content>
 			<Tabs.Content value="discord">
 				{#if $user.data.discord}
-					<a
-						href="https://discord.com/oauth2/authorize?client_id=1071500325338488843&response_type=code&redirect_uri=https%3A%2F%2Fapi.demonlistvn.com%2Fauth%2Fcallback%2Fdiscord&scope=identify"
-					>
-						<Button class="w-full" variant="outline">Change linked account</Button>
-					</a>
+					<Button class="w-full" variant="outline" disabled>Linked</Button>
 				{:else}
 					<a
 						href="https://discord.com/oauth2/authorize?client_id=1071500325338488843&response_type=code&redirect_uri=https%3A%2F%2Fapi.demonlistvn.com%2Fauth%2Fcallback%2Fdiscord&scope=identify"
