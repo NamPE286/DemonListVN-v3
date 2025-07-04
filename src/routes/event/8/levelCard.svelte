6c922cda1e75c261f49925335723a9c2fc2d06b1
NamPE286 <nambuihung654@gmail.com>
2025-07-04 22:22:14 +0700

    16:9 thumb

--- src/routes/event/8/levelCard.svelte
+++ src/routes/event/8/levelCard.svelte
@@ -6,15 +6,20 @@
 </script>
 
 <Card.Root class="mb-[10px] ml-auto mr-auto flex w-[1000px] max-w-full">
-	<img
-		src={`https://img.youtube.com/vi/${level.videoID}/0.jpg`}
-		alt="level"
-		class="h-[100px] w-[200px] rounded-xl object-cover"
-	/>
+	<a href={`https://www.youtube.com/watch?v=${level.videoID}`}>
+		<img
+			src={`https://img.youtube.com/vi/${level.videoID}/0.jpg`}
+			alt="level"
+			class="h-[100px] w-[177px] rounded-xl object-cover"
+		/>
+	</a>
 	<Card.Content class="mt-[22.5px] flex flex-col justify-center">
 		<div class="flex items-center gap-[10px]">
 			<h2 class="text-xl font-bold">{level.name}</h2>
-            <span class="bg-[var(--textColor)] text-[var(--textColorInverted)] pl-[5px] pr-[5px] text-sm rounded-sm">{level.point}pt</span>
+			<span
+				class="rounded-sm bg-[var(--textColor)] pl-[5px] pr-[5px] text-sm text-[var(--textColorInverted)]"
+				>{level.point}pt</span
+			>
 		</div>
 		<p>by {level.creator} - ID: {level.id}</p>
 	</Card.Content>
