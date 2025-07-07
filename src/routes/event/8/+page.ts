2966c938da9766e27f6981815c7b6e16cbfd154a
NamPE286 <nambuihung654@gmail.com>
2025-07-07 22:49:45 +0700

    merge

--- src/routes/event/8/+page.ts
+++
@@ -1,5 +0,0 @@
-export async function load({ params, url, fetch }) {
-    const event = await (await fetch(`${import.meta.env.VITE_API_URL}/event/8`)).json()
-
-    return event
-};
--- src/routes/event/[id]/+page.svelte
+++ src/routes/event/[id]/+page.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import Markdown from '$lib/components/markdown.svelte';
+	import * as Tabs from '$lib/components/ui/tabs';
 	import { user } from '$lib/client';
 	import EventBanner from '../eventBanner.svelte';
+	import Markdown from '$lib/components/markdown.svelte';
+	import LevelTab from './levelTab.svelte';
+	import Leaderboard from './leaderboard.svelte';
+	import { onMount } from 'svelte';
 	import ParticipateButton from './participateButton.svelte';
+	import type { Level } from './type';
 
 	export let data: PageData;
-	
+
+	let levels: Level[] = [];
+
 	function isEventStarted() {
 		return new Date(data.start) <= new Date();
 	}
+
+	onMount(async () => {
+		levels = await (await fetch(`${import.meta.env.VITE_API_URL}/event/8/levels`)).json();
+	});
 </script>
 
 <svelte:head>
@@ -19,12 +30,35 @@
 <EventBanner {data} />
 
 {#if isEventStarted() || ($user.loggedIn && $user.data.isAdmin)}
-	<ParticipateButton {data} />
-	<div class="content markdown">
-		{#if data.content}
-			<Markdown content={data.content} />
-		{/if}
-	</div>
+	{#if data.isContest}
+		<Tabs.Root value="detail" class="mt-[20px] flex flex-col items-center">
+			<Tabs.List>
+				<Tabs.Trigger value="detail">Detail</Tabs.Trigger>
+				<Tabs.Trigger value="levels">Levels</Tabs.Trigger>
+				<Tabs.Trigger value="leaderboard">Leaderboard</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="detail">
+				<div class="content markdown">
+					{#if data.content}
+						<Markdown content={data.content} />
+					{/if}
+				</div>
+			</Tabs.Content>
+			<Tabs.Content value="levels" class="mt-[20px] w-full pl-[10px] pr-[10px]">
+				<LevelTab {levels} event={data} />
+			</Tabs.Content>
+			<Tabs.Content value="leaderboard" class="mt-[20px] w-full pl-[10px] pr-[10px]">
+				<Leaderboard {levels} event={data} />
+			</Tabs.Content>
+		</Tabs.Root>
+	{:else}
+		<ParticipateButton {data} />
+		<div class="content markdown">
+			{#if data.content}
+				<Markdown content={data.content} />
+			{/if}
+		</div>
+	{/if}
 {/if}
 
 <style lang="scss">
