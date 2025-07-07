1999ccb867a1d1a7b1095bfb351d8adc525bac42
NamPE286 <nambuihung654@gmail.com>
2025-07-07 22:55:37 +0700

    update enabled const for ads

--- src/lib/components/ads.svelte
+++ src/lib/components/ads.svelte
@@ -2,37 +2,44 @@
 	import { onMount } from 'svelte';
 
 	export let dataAdFormat = 'auto';
+	const enabled = false;
 	let mounted = false;
 
-	// onMount(() => {
-	// 	if (mounted) {
-	// 		return;
-	// 	}
+	onMount(() => {
+		if (!enabled) {
+			return;
+		}
 
-	// 	mounted = true;
+		if (mounted) {
+			return;
+		}
 
-	// 	try {
-	// 		// @ts-ignore
-	// 		(window.adsbygoogle = window.adsbygoogle || []).push({});
-	// 	} catch (err) {
-	// 		console.error(err);
-	// 	}
-	// });
+		mounted = true;
+
+		try {
+			// @ts-ignore
+			(window.adsbygoogle = window.adsbygoogle || []).push({});
+		} catch (err) {
+			console.error(err);
+		}
+	});
 </script>
 
-<!-- <div class="text-center">
-	<ins
-		class="adsbygoogle ad"
-		style="display:block"
-		data-ad-client="ca-pub-4605218533506777"
-		data-ad-slot="7284584064"
-		data-ad-format={dataAdFormat}
-		data-full-width-responsive="true"
-	></ins>
-</div> -->
+{#if enabled}
+	<div class="text-center">
+		<ins
+			class="adsbygoogle ad"
+			style="display:block"
+			data-ad-client="ca-pub-4605218533506777"
+			data-ad-slot="7284584064"
+			data-ad-format={dataAdFormat}
+			data-full-width-responsive="true"
+		></ins>
+	</div>
+{/if}
 
 <style>
-	/* .ad {
+	.ad {
 		margin-inline: auto;
 		margin-top: 20px;
 		margin-bottom: 20px;
@@ -50,5 +57,5 @@
 		.ad {
 			width: 728px;
 		}
-	} */
+	}
 </style>
