bd07bfb58dcd32d125e10e52d4be53d0fd1ad513
NamPE286 <nambuihung654@gmail.com>
2025-07-09 00:33:33 +0700

    redirect

--- src/routes/player/[uid]/medal.svelte
+++ src/routes/player/[uid]/medal.svelte
@@ -3,15 +3,21 @@
 	console.log(medal.medalID);
 </script>
 
-<div class="flex flex-col items-center w-[200px] hover:bg-[hsl(var(--secondary))] p-[15px] rounded-xl transition duration-200 ease-in-out">
-    <img
-        class="h-[150px] w-[150px] object-cover"
-        src={`https://qdwpenfblwdmhywwszzj.supabase.co/storage/v1/object/public/medals/${medal.medalID}.webp`}
-        alt=""
-    />
-    <h3 class="font-bold text-xl mt-[10px] mb-[10px] text-center">{medal.name}</h3>
-    {#if medal.content}
-        <p class='text-center'>{medal.content}</p>
-    {/if}
-    <p class='text-[var(--textColor2)] mt-[10px]'>Achieved on {new Date(medal.created_at).toLocaleDateString('vi-vn')}</p>
-</div>
+<a href={medal.redirect ? medal.redirect : "#!"}>
+	<div
+		class="flex w-[200px] flex-col items-center rounded-xl p-[15px] transition duration-200 ease-in-out hover:bg-[hsl(var(--secondary))]"
+	>
+		<img
+			class="h-[150px] w-[150px] object-cover"
+			src={`https://qdwpenfblwdmhywwszzj.supabase.co/storage/v1/object/public/medals/${medal.medalID}.webp`}
+			alt=""
+		/>
+		<h3 class="mb-[10px] mt-[10px] text-center text-xl font-bold">{medal.name}</h3>
+		{#if medal.content}
+			<p class="text-center">{medal.content}</p>
+		{/if}
+		<p class="mt-[10px] text-[var(--textColor2)]">
+			Achieved on {new Date(medal.created_at).toLocaleDateString('vi-vn')}
+		</p>
+	</div>
+</a>
