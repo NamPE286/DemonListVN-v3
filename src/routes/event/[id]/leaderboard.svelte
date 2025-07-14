3f475cc7b0041ec45df54739a8f0ee857089847d
NamPE286 <nambuihung654@gmail.com>
2025-07-15 04:06:09 +0700

    reveal mode

--- src/routes/event/[id]/leaderboard.svelte
+++ src/routes/event/[id]/leaderboard.svelte
@@ -17,14 +17,15 @@
 	import { ExclamationTriangle } from 'svelte-radix';
 	import { flip } from 'svelte/animate';
 	import { cn } from '$lib/utils.js';
-	import { bounceInOut } from 'svelte/easing';
 
 	export let levels: (Level | null)[];
 	export let event: any;
 
 	let leaderboard: any[] = [];
+	let leaderboard1: any[] = [];
 	let refreshing = false;
 	let updateData: any = null;
+	let revealMode = false;
 
 	function indexToRoman(num: number): string {
 		const romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
@@ -43,7 +44,7 @@
 		let res: number = 0;
 
 		for (const i of records) {
-			if (i == null || i.accepted === false) {
+			if (i == null || i == 'revealed' || i.accepted === false) {
 				continue;
 			}
 
@@ -64,7 +65,7 @@
 	}
 
 	function getPoint(record: any, index: number, checkAcp = false) {
-		if (record === null || (record.accepted === false && checkAcp)) {
+		if (record === null || record == 'revealed' || (record.accepted === false && checkAcp)) {
 			return 0;
 		}
 
@@ -98,7 +99,15 @@
 					}
 				})
 			).json();
+
+			if ($user.loggedIn && $user.data.isAdmin) {
+				leaderboard1 = await (
+					await fetch(`${import.meta.env.VITE_API_URL}/event/${event.id}/leaderboard`)
+				).json();
+			}
+
 			refreshing = false;
+			revealMode = false;
 		};
 
 		if (noti) {
@@ -179,6 +188,49 @@
 		URL.revokeObjectURL(url);
 	}
 
+	function reveal(uid: string, levelIndex: number, rank: number) {
+		let res;
+
+		for (const i of leaderboard1) {
+			if (i.uid == uid) {
+				res = i;
+				break;
+			}
+		}
+
+		if (!res.eventRecords[levelIndex]) {
+			leaderboard[rank].eventRecords[levelIndex] = 'revealed';
+		} else {
+			leaderboard[rank].eventRecords[levelIndex] = res.eventRecords[levelIndex];
+		}
+
+		leaderboard.sort((a, b) => {
+			const x = a.eventRecords.reduce((sum: any, record: any, index: any) => {
+				return (
+					sum +
+					(record && (record.accepted === null || record.accepted === true)
+						? levels[index]!.point * record.progress
+						: 0)
+				);
+			}, 0);
+
+			const y = b.eventRecords.reduce((sum: any, record: any, index: any) => {
+				return (
+					sum +
+					(record && (record.accepted === null || record.accepted === true)
+						? levels[index]!.point * record.progress
+						: 0)
+				);
+			}, 0);
+
+			if (x == y && x != 0) {
+				return getPenalty(a.eventRecords, 1) - getPenalty(b.eventRecords, 1);
+			}
+
+			return y - x;
+		});
+	}
+
 	onMount(async () => {
 		await refresh();
 	});
@@ -211,9 +263,22 @@
 	<Button class="w-fit" variant="outline" disabled={refreshing} on:click={() => refresh(true)}>
 		<Reload size={16} />
 	</Button>
+	{#if $user.loggedIn && $user.data.isAdmin}
+		<Button
+			on:click={() => {
+				revealMode = !revealMode;
+				[leaderboard, leaderboard1] = [leaderboard1, leaderboard];
+			}}
+			disabled={leaderboard.length === 0}
+			class="w-fit"
+			variant="outline"
+		>
+			Switch to {revealMode ? 'Normal' : 'Reveal'} Mode
+		</Button>
+	{/if}
 </div>
 <div class="relative">
-	<div class="sticky top-[55px] bg-[hsl(var(--background))] z-10">
+	<div class="sticky top-[55px] z-10 bg-[hsl(var(--background))]">
 		<Table.Root class="ml-auto mr-auto w-[1500px] max-w-full">
 			<Table.Header>
 				<Table.Row>
@@ -269,142 +334,174 @@
 						</Tooltip.Root>
 					</Table.Cell>
 					{#each player.eventRecords as record, index}
-						<Table.Cell class="w-[75px] text-center">
-							{#if getPoint(record, index) == 0}
-								{#if new Date(event.freeze) > new Date()}
-									0
+						{#if !revealMode}
+							<Table.Cell class="w-[75px] text-center">
+								{#if getPoint(record, index) == 0}
+									{#if new Date(event.freeze) > new Date()}
+										0
+									{:else}
+										-
+									{/if}
 								{:else}
-									-
-								{/if}
-							{:else}
-								<Dialog.Root>
-									<Dialog.Trigger
-										on:click={() => {
-											updateData = structuredClone(record);
-											delete updateData.eventLevels;
+									<Dialog.Root>
+										<Dialog.Trigger
+											on:click={() => {
+												updateData = structuredClone(record);
+												delete updateData.eventLevels;
 
-											updateData.accepted = {
-												disabled: false,
-												label: (() => {
-													if (updateData.accepted === null) {
-														return 'Undecided';
-													}
+												updateData.accepted = {
+													disabled: false,
+													label: (() => {
+														if (updateData.accepted === null) {
+															return 'Undecided';
+														}
 
-													return updateData.accepted ? 'Accepted' : 'Rejected';
-												})(),
-												value: updateData.accepted
-											};
+														return updateData.accepted ? 'Accepted' : 'Rejected';
+													})(),
+													value: updateData.accepted
+												};
 
-											updateData.created_at = new Date(updateData.created_at)
-												.toISOString()
-												.slice(0, 16);
-										}}
-									>
-										{#if record && record.accepted === false}
-											<s>
+												updateData.created_at = new Date(updateData.created_at)
+													.toISOString()
+													.slice(0, 16);
+											}}
+										>
+											{#if record && record.accepted === false}
+												<s>
+													{getPoint(record, index)}<br />
+													<span class="text-[11px] opacity-50">
+														{record ? `${record.progress}%` : ''}
+													</span>
+												</s>
+											{:else if record && record.accepted === null}
+												{getPoint(record, index)}*<br />
+												<span class="text-[11px] opacity-50">
+													{record ? `${record.progress}%` : ''}
+												</span>
+											{:else}
 												{getPoint(record, index)}<br />
 												<span class="text-[11px] opacity-50">
 													{record ? `${record.progress}%` : ''}
 												</span>
-											</s>
-										{:else if record && record.accepted === null}
-											{getPoint(record, index)}*<br />
-											<span class="text-[11px] opacity-50">
-												{record ? `${record.progress}%` : ''}
-											</span>
-										{:else}
+											{/if}
+										</Dialog.Trigger>
+										<Dialog.Content>
+											<Dialog.Header>
+												<Dialog.Title>Record's Detail</Dialog.Title>
+											</Dialog.Header>
+											<div class="flex flex-col gap-0">
+												<section>
+													<span class="font-bold">Submitted at: </span>
+													{new Date(record.created_at).toLocaleString('vi-vn')}
+												</section>
+												<section>
+													<span class="font-bold">Video's Link: </span><a
+														href={record.videoLink}
+														class="text-[#95bdf7]"
+														target="_blank">Link</a
+													>
+												</section>
+												<section>
+													<span class="font-bold">Raw: </span>
+													{#if record.raw}
+														<a href={record.raw} class="text-[#95bdf7]" target="_blank">Link</a>
+													{:else}
+														(Not provided)
+													{/if}
+												</section>
+												{#if record.accepted === null}
+													<section class="mt-[10px] text-[13px] opacity-50">
+														* This record legitimacy is not verified
+													</section>
+												{:else if record.accepted === false}
+													<section class="mt-[10px] text-[13px] opacity-50">
+														* This record is not counted
+													</section>
+												{/if}
+
+												{#if $user.loggedIn && $user.data.isAdmin && updateData}
+													<div class="grid gap-4 py-4">
+														<div class="grid grid-cols-4 items-center gap-4">
+															<Label for="name" class="text-right">Submitted at</Label>
+															<Input
+																type="datetime-local"
+																bind:value={updateData.created_at}
+																class="col-span-3"
+															/>
+														</div>
+														<div class="grid grid-cols-4 items-center gap-4">
+															<Label for="name" class="text-right">Progress</Label>
+															<Input
+																type="number"
+																bind:value={updateData.progress}
+																class="col-span-3"
+															/>
+														</div>
+														<div class="grid grid-cols-4 items-center gap-4">
+															<Label for="name" class="text-right">Video's Link</Label>
+															<Input bind:value={updateData.videoLink} class="col-span-3" />
+														</div>
+														{#if levels[index] && levels[index].needRaw}
+															<div class="grid grid-cols-4 items-center gap-4">
+																<Label for="name" class="text-right">Raw</Label>
+																<Input bind:value={updateData.raw} class="col-span-3" />
+															</div>
+														{/if}
+														<div class="grid grid-cols-4 items-center gap-4">
+															<Label for="name" class="text-right">Accept state</Label>
+															<Select.Root bind:selected={updateData.accepted}>
+																<Select.Trigger class="col-span-3">
+																	<Select.Value placeholder="Select a platform" />
+																</Select.Trigger>
+																<Select.Content>
+																	<Select.Group>
+																		<Select.Item value={null}>Undecided</Select.Item>
+																		<Select.Item value={true}>Accepted</Select.Item>
+																		<Select.Item value={false}>Rejected</Select.Item>
+																	</Select.Group>
+																</Select.Content>
+																<Select.Input name="platform" value={true} />
+															</Select.Root>
+														</div>
+														<Button on:click={() => updateRecord(record)}>Update</Button>
+													</div>
+												{/if}
+											</div>
+										</Dialog.Content>
+									</Dialog.Root>
+								{/if}
+							</Table.Cell>
+						{:else}
+							<Table.Cell
+								class="w-[75px] h-[50px] text-center cursor-pointer"
+								on:click={() => reveal(player.uid, index, rank)}
+							>
+								{#if getPoint(record, index)}
+									{#if record && record.accepted === false}
+										<s>
 											{getPoint(record, index)}<br />
 											<span class="text-[11px] opacity-50">
 												{record ? `${record.progress}%` : ''}
 											</span>
-										{/if}
-									</Dialog.Trigger>
-									<Dialog.Content>
-										<Dialog.Header>
-											<Dialog.Title>Record's Detail</Dialog.Title>
-										</Dialog.Header>
-										<div class="flex flex-col gap-0">
-											<section>
-												<span class="font-bold">Submitted at: </span>
-												{new Date(record.created_at).toLocaleString('vi-vn')}
-											</section>
-											<section>
-												<span class="font-bold">Video's Link: </span><a
-													href={record.videoLink}
-													class="text-[#95bdf7]"
-													target="_blank">Link</a
-												>
-											</section>
-											<section>
-												<span class="font-bold">Raw: </span>
-												{#if record.raw}
-													<a href={record.raw} class="text-[#95bdf7]" target="_blank">Link</a>
-												{:else}
-													(Not provided)
-												{/if}
-											</section>
-											{#if record.accepted === null}
-												<section class="mt-[10px] text-[13px] opacity-50">
-													* This record legitimacy is not verified
-												</section>
-											{:else if record.accepted === false}
-												<section class="mt-[10px] text-[13px] opacity-50">
-													* This record is not counted
-												</section>
-											{/if}
-
-											{#if $user.loggedIn && $user.data.isAdmin && updateData}
-												<div class="grid gap-4 py-4">
-													<div class="grid grid-cols-4 items-center gap-4">
-														<Label for="name" class="text-right">Submitted at</Label>
-														<Input
-															type="datetime-local"
-															bind:value={updateData.created_at}
-															class="col-span-3"
-														/>
-													</div>
-													<div class="grid grid-cols-4 items-center gap-4">
-														<Label for="name" class="text-right">Progress</Label>
-														<Input
-															type="number"
-															bind:value={updateData.progress}
-															class="col-span-3"
-														/>
-													</div>
-													<div class="grid grid-cols-4 items-center gap-4">
-														<Label for="name" class="text-right">Video's Link</Label>
-														<Input bind:value={updateData.videoLink} class="col-span-3" />
-													</div>
-													{#if levels[index] && levels[index].needRaw}
-														<div class="grid grid-cols-4 items-center gap-4">
-															<Label for="name" class="text-right">Raw</Label>
-															<Input bind:value={updateData.raw} class="col-span-3" />
-														</div>
-													{/if}
-													<div class="grid grid-cols-4 items-center gap-4">
-														<Label for="name" class="text-right">Accept state</Label>
-														<Select.Root bind:selected={updateData.accepted}>
-															<Select.Trigger class="col-span-3">
-																<Select.Value placeholder="Select a platform" />
-															</Select.Trigger>
-															<Select.Content>
-																<Select.Group>
-																	<Select.Item value={null}>Undecided</Select.Item>
-																	<Select.Item value={true}>Accepted</Select.Item>
-																	<Select.Item value={false}>Rejected</Select.Item>
-																</Select.Group>
-															</Select.Content>
-															<Select.Input name="platform" value={true} />
-														</Select.Root>
-													</div>
-													<Button on:click={() => updateRecord(record)}>Update</Button>
-												</div>
-											{/if}
-										</div>
-									</Dialog.Content>
-								</Dialog.Root>
-							{/if}
-						</Table.Cell>
+										</s>
+									{:else if record && record.accepted === null}
+										{getPoint(record, index)}*<br />
+										<span class="text-[11px] opacity-50">
+											{record ? `${record.progress}%` : ''}
+										</span>
+									{:else}
+										{getPoint(record, index)}<br />
+										<span class="text-[11px] opacity-50">
+											{record ? `${record.progress}%` : ''}
+										</span>
+									{/if}
+								{:else if record == 'revealed'}
+									0
+								{:else}
+									-
+								{/if}
+							</Table.Cell>
+						{/if}
 					{/each}
 				</tr>
 			{/each}
