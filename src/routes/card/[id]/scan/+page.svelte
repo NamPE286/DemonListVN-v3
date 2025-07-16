c47c9c0b348a3e1d9d47a566875bde873403d8d2
NamPE286 <nambuihung654@gmail.com>
2025-07-16 15:59:07 +0700

    remove animation

--- src/routes/card/[id]/scan/+page.svelte
+++ src/routes/card/[id]/scan/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { PageData } from '../$types';
-	import { fly } from 'svelte/transition';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 	import { getTitle, user } from '$lib/client';
@@ -12,14 +11,11 @@
 	import { badgeVariants } from '$lib/components/ui/badge';
 	import { getExpLevel } from '$lib/client/getExpLevel';
 	import * as Tooltip from '$lib/components/ui/tooltip';
-	import { onMount } from 'svelte';
-	import { load } from './+page';
 
 	export let data: PageData;
 
 	let isBannerFailedToLoad = false;
 	let exp = data.owner ? data.players.exp + data.players.extraExp : 0;
-	let loaded = false;
 
 	async function link() {
 		toast.promise(
@@ -39,10 +35,6 @@
 			}
 		);
 	}
-
-	onMount(() => {
-		loaded = true;
-	});
 </script>
 
 <svelte:head>
@@ -59,144 +51,141 @@
 	/>
 	<p class="text-[12px] opacity-50">ID: {data.id}</p>
 	<h3 class="text-xl font-bold">{data.name} Card</h3>
-	{#if loaded}
-		<div class="relative z-0 w-full" transition:fly={{ y: -250, duration: 1000, opacity: 100 }}>
-			{#if data.activationDate == null}
-				<div class="text-center">
-					<p>This card is not activated.</p>
-					<p>Please contact with a moderator to activate this card.</p>
-				</div>
-			{:else if data.owner == null}
-				<div class="flex justify-center">
-					{#if $user.loggedIn}
-						<AlertDialog.Root>
-							<AlertDialog.Trigger>
-								<Button class="w-full">Link card to {$user.data.name}</Button>
-							</AlertDialog.Trigger>
-							<AlertDialog.Content>
-								<AlertDialog.Header>
-									<AlertDialog.Title
-										>Link card to this account ({$user.data.name})?</AlertDialog.Title
-									>
-									<AlertDialog.Description>
-										This will permanently link the card to this account ({$user.data.name}) and
-										cannot be unlinked or transfered.
-									</AlertDialog.Description>
-								</AlertDialog.Header>
-								<AlertDialog.Footer>
-									<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-									<AlertDialog.Action on:click={link}>Continue</AlertDialog.Action>
-								</AlertDialog.Footer>
-							</AlertDialog.Content>
-						</AlertDialog.Root>
-					{:else}
-						<Skeleton class="h-[35px] w-[150px]" />
-					{/if}
-				</div>
-			{:else if data.owner}
-				<Card.Root
-					class="mt-[20px] w-full"
-					style={isSupporterActive(data.players.supporterUntil)
-						? `background-color: ${data.players.bgColor}; border-color: ${data.players.borderColor}; ${data.players.bgColor ? 'color: white' : ''}`
-						: ''}
-				>
-					{#if isSupporterActive(data.players.supporterUntil) && !isBannerFailedToLoad}
-						<img
-							on:error={() => {
-								isBannerFailedToLoad = true;
-							}}
-							class="bgGradient absolute z-0 rounded-xl object-cover"
-							src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/banners/${data.players.uid}${
-								data.players.isBannerGif ? '.gif' : '.jpg'
-							}`}
-							alt=""
-						/>
-					{/if}
-					<Card.Content class="z-1 relative mt-[22.5px] flex flex-col gap-[15px]">
-						<div class="flex items-center gap-[10px]">
-							<Avatar.Root>
-								<Avatar.Image
-									class="object-cover"
-									src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/avatars/${data.players.uid}${
-										isSupporterActive(data.players.supporterUntil) && data.players.isAvatarGif
-											? '.gif'
-											: '.jpg'
-									}`}
-									alt=""
-								/>
-								<Avatar.Fallback>{data.players.name[0]}</Avatar.Fallback>
-							</Avatar.Root>
-							{#if data.players.clan}
-								<a
-									href={`/clan/${data.players.clan}`}
-									class={badgeVariants({ variant: 'secondary' })}
-									style={`background-color: ${data.players.clans.tagBgColor}; color: ${data.players.clans.tagTextColor};`}
-									>{data.players.clans.tag}</a
+	<div class="relative z-0 w-full">
+		{#if data.activationDate == null}
+			<div class="text-center">
+				<p>This card is not activated.</p>
+				<p>Please contact with a moderator to activate this card.</p>
+			</div>
+		{:else if data.owner == null}
+			<div class="flex justify-center">
+				{#if $user.loggedIn}
+					<AlertDialog.Root>
+						<AlertDialog.Trigger>
+							<Button class="w-full">Link card to {$user.data.name}</Button>
+						</AlertDialog.Trigger>
+						<AlertDialog.Content>
+							<AlertDialog.Header>
+								<AlertDialog.Title>Link card to this account ({$user.data.name})?</AlertDialog.Title
 								>
-							{/if}
-							<a href={`/player/${data.players.uid}`}>
-								<h4 class="font-semibold">
-									{#if isSupporterActive(data.players.supporterUntil)}
-										<span class="text-yellow-500">
-											{data.players.name}
-										</span>
-									{:else}
+								<AlertDialog.Description>
+									This will permanently link the card to this account ({$user.data.name}) and cannot
+									be unlinked or transfered.
+								</AlertDialog.Description>
+							</AlertDialog.Header>
+							<AlertDialog.Footer>
+								<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+								<AlertDialog.Action on:click={link}>Continue</AlertDialog.Action>
+							</AlertDialog.Footer>
+						</AlertDialog.Content>
+					</AlertDialog.Root>
+				{:else}
+					<Skeleton class="h-[35px] w-[150px]" />
+				{/if}
+			</div>
+		{:else if data.owner}
+			<Card.Root
+				class="mt-[20px] w-full"
+				style={isSupporterActive(data.players.supporterUntil)
+					? `background-color: ${data.players.bgColor}; border-color: ${data.players.borderColor}; ${data.players.bgColor ? 'color: white' : ''}`
+					: ''}
+			>
+				{#if isSupporterActive(data.players.supporterUntil) && !isBannerFailedToLoad}
+					<img
+						on:error={() => {
+							isBannerFailedToLoad = true;
+						}}
+						class="bgGradient absolute z-0 rounded-xl object-cover"
+						src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/banners/${data.players.uid}${
+							data.players.isBannerGif ? '.gif' : '.jpg'
+						}`}
+						alt=""
+					/>
+				{/if}
+				<Card.Content class="z-1 relative mt-[22.5px] flex flex-col gap-[15px]">
+					<div class="flex items-center gap-[10px]">
+						<Avatar.Root>
+							<Avatar.Image
+								class="object-cover"
+								src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/avatars/${data.players.uid}${
+									isSupporterActive(data.players.supporterUntil) && data.players.isAvatarGif
+										? '.gif'
+										: '.jpg'
+								}`}
+								alt=""
+							/>
+							<Avatar.Fallback>{data.players.name[0]}</Avatar.Fallback>
+						</Avatar.Root>
+						{#if data.players.clan}
+							<a
+								href={`/clan/${data.players.clan}`}
+								class={badgeVariants({ variant: 'secondary' })}
+								style={`background-color: ${data.players.clans.tagBgColor}; color: ${data.players.clans.tagTextColor};`}
+								>{data.players.clans.tag}</a
+							>
+						{/if}
+						<a href={`/player/${data.players.uid}`}>
+							<h4 class="font-semibold">
+								{#if isSupporterActive(data.players.supporterUntil)}
+									<span class="text-yellow-500">
 										{data.players.name}
-									{/if}
-								</h4>
-							</a>
-						</div>
-						<div class="flex flex-col gap-[2px]">
-							<div class="rating">
-								<div class="flex justify-center">
-									<div class="leftCol">
-										<b>Lv.{getExpLevel(exp).level}</b>
-									</div>
-								</div>
-								<div class="progressBar">
-									<div class="progress" style={`width: ${getExpLevel(exp).progress}%`}>
-										<b>{getExpLevel(exp).progress}%</b>
-									</div>
-								</div>
-							</div>
-							<div class="rating">
-								<Tooltip.Root>
-									<Tooltip.Trigger>
-										<div class="leftCol">
-											<div
-												class="title text-white"
-												style={`background-color: ${getTitle('dl', data.players)?.color}`}
-											>
-												{data.players.rating}
-											</div>
-										</div>
-									</Tooltip.Trigger>
-									<Tooltip.Content>{getTitle('dl', data.players)?.fullTitle}</Tooltip.Content>
-								</Tooltip.Root>
-								<div class="rankWrapper">
-									Demon List rating
-									<div class="rank">
-										#{data.players.overallRank}
-									</div>
-								</div>
-							</div>
-							<div class="rating">
+									</span>
+								{:else}
+									{data.players.name}
+								{/if}
+							</h4>
+						</a>
+					</div>
+					<div class="flex flex-col gap-[2px]">
+						<div class="rating">
+							<div class="flex justify-center">
 								<div class="leftCol">
-									<div class="title">{data.players.totalFLpt}</div>
+									<b>Lv.{getExpLevel(exp).level}</b>
 								</div>
-								<div class="rankWrapper">
-									Total Featured List point
-									<div class="rank">
-										#{data.players.flrank}
-									</div>
+							</div>
+							<div class="progressBar">
+								<div class="progress" style={`width: ${getExpLevel(exp).progress}%`}>
+									<b>{getExpLevel(exp).progress}%</b>
 								</div>
 							</div>
 						</div>
-					</Card.Content>
-				</Card.Root>
-			{/if}
-		</div>
-	{/if}
+						<div class="rating">
+							<Tooltip.Root>
+								<Tooltip.Trigger>
+									<div class="leftCol">
+										<div
+											class="title text-white"
+											style={`background-color: ${getTitle('dl', data.players)?.color}`}
+										>
+											{data.players.rating}
+										</div>
+									</div>
+								</Tooltip.Trigger>
+								<Tooltip.Content>{getTitle('dl', data.players)?.fullTitle}</Tooltip.Content>
+							</Tooltip.Root>
+							<div class="rankWrapper">
+								Demon List rating
+								<div class="rank">
+									#{data.players.overallRank}
+								</div>
+							</div>
+						</div>
+						<div class="rating">
+							<div class="leftCol">
+								<div class="title">{data.players.totalFLpt}</div>
+							</div>
+							<div class="rankWrapper">
+								Total Featured List point
+								<div class="rank">
+									#{data.players.flrank}
+								</div>
+							</div>
+						</div>
+					</div>
+				</Card.Content>
+			</Card.Root>
+		{/if}
+	</div>
 </div>
 
 <style lang="scss">
