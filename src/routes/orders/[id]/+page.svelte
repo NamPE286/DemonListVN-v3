f1a64f423853510c72331d3e418216dbe236f1c5
NamPE286 <nambuihung654@gmail.com>
2025-07-21 04:13:06 +0700

    protect order data

--- src/routes/orders/[id]/+page.svelte
+++ src/routes/orders/[id]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import Title from '$lib/components/Title.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Table from '$lib/components/ui/table/index.js';
@@ -7,8 +6,10 @@
 	import PlayerHoverCard from '$lib/components/playerHoverCard.svelte';
 	import { user } from '$lib/client';
 	import { toast } from 'svelte-sonner';
+	import { page } from '$app/state';
+	import { onMount } from 'svelte';
 
-	export let data: PageData;
+	let data: any = null;
 
 	function cancellable() {
 		if (data.state == 'CANCELLED' || data.paymentMethod == 'Bank Transfer') {
@@ -32,6 +33,32 @@
 		toast.loading('Cancelling...');
 		window.location.href = `${import.meta.env.VITE_API_URL}/payment/cancelled?orderCode=${data.id}`;
 	}
+
+	async function fetchData() {
+		data = await (
+			await fetch(`${import.meta.env.VITE_API_URL}/order/${page.params.id}`, {
+				method: 'GET',
+				headers: {
+					Authorization: 'Bearer ' + (await $user.token())
+				}
+			})
+		).json();
+
+		data.orderTracking.push({
+			created_at: data.created_at,
+			content: 'Order created'
+		});
+	}
+
+	onMount(async () => {
+		user.subscribe(async (currentUser) => {
+			if (currentUser) {
+				await fetchData();
+			}
+		});
+
+		await fetchData();
+	});
 </script>
 
 <svelte:head>
@@ -40,163 +67,165 @@
 
 <Title value="Order Details" />
 
-<div
-	class="mb-[50px] ml-auto mr-auto mt-[30px] flex w-[800px] max-w-full flex-col gap-[10px] pl-[15px] pr-[15px]"
->
-	<Card.Root class="w-full">
-		<Card.Header>
-			<Card.Title>Detail</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<p>Order ID: {data.id}</p>
-			<p>Status: <b>{data.state} {data.delivered ? 'and DELIVERED' : ''}</b></p>
-			<p>Payment method: {data.paymentMethod}</p>
-			{#if !data.productID}
-				<div class="flex gap-[10px]">
-					<p>Shipping info:</p>
-					<div>
-						<p>{data.recipentName}</p>
-						<p>{data.address}</p>
-						<p>+84 {String(data.phone).replace(/(\d{3})(\d{3})(\d{3})/, '$1 $2 $3')}</p>
-					</div>
-				</div>
-			{:else}
-				<div class="flex gap-[5px]">
-					Recipent:
-					{#if data.giftTo}
-						<PlayerHoverCard player={data.players} />
-					{:else}
-						<PlayerHoverCard player={$user.data} />
-					{/if}
-				</div>
-			{/if}
-		</Card.Content>
-	</Card.Root>
-	<Card.Root class="w-full">
-		<Card.Header>
-			<Card.Title>Items</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<Table.Root>
-				<Table.Body>
-					{#each data.orderItems as item}
-						<Table.Row>
-							<div class="border-box flex gap-[10px] p-[10px]">
-								<a href={`/store/product/${item.productID}`}>
-									<img
-										class="h-[65px] w-[65px] rounded-lg object-cover"
-										src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/products/${item.productID}/0.webp`}
-										alt="product"
-									/>
-								</a>
-								<div>
-									<a href={`/store/product/${item.productID}`}>
-										<h3 class="text-lg font-semibold">
-											{item.products.name}
-										</h3>
-									</a>
-									<p>
-										Qty: {item.quantity}
-									</p>
-								</div>
-								<div class="ml-auto flex items-center">
-									{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
-										item.quantity * item.products.price
-									)}
-								</div>
-							</div>
-						</Table.Row>
-					{/each}
-				</Table.Body>
-			</Table.Root>
-			<div class="mt-[15px]">
-				<div class="flex">
-					<p>Subtotal</p>
-					<p class="ml-auto">
-						<b>
-							{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
-								data.amount
-							)}
-						</b>
-					</p>
-				</div>
+{#if data}
+	<div
+		class="mb-[50px] ml-auto mr-auto mt-[30px] flex w-[800px] max-w-full flex-col gap-[10px] pl-[15px] pr-[15px]"
+	>
+		<Card.Root class="w-full">
+			<Card.Header>
+				<Card.Title>Detail</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p>Order ID: {data.id}</p>
+				<p>Status: <b>{data.state} {data.delivered ? 'and DELIVERED' : ''}</b></p>
+				<p>Payment method: {data.paymentMethod}</p>
 				{#if !data.productID}
+					<div class="flex gap-[10px]">
+						<p>Shipping info:</p>
+						<div>
+							<p>{data.recipentName}</p>
+							<p>{data.address}</p>
+							<p>+84 {String(data.phone).replace(/(\d{3})(\d{3})(\d{3})/, '$1 $2 $3')}</p>
+						</div>
+					</div>
+				{:else}
+					<div class="flex gap-[5px]">
+						Recipent:
+						{#if data.giftTo}
+							<PlayerHoverCard player={data.players} />
+						{:else}
+							<PlayerHoverCard player={$user.data} />
+						{/if}
+					</div>
+				{/if}
+			</Card.Content>
+		</Card.Root>
+		<Card.Root class="w-full">
+			<Card.Header>
+				<Card.Title>Items</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Table.Root>
+					<Table.Body>
+						{#each data.orderItems as item}
+							<Table.Row>
+								<div class="border-box flex gap-[10px] p-[10px]">
+									<a href={`/store/product/${item.productID}`}>
+										<img
+											class="h-[65px] w-[65px] rounded-lg object-cover"
+											src={`${import.meta.env.VITE_SUPABASE_API_URL}/storage/v1/object/public/products/${item.productID}/0.webp`}
+											alt="product"
+										/>
+									</a>
+									<div>
+										<a href={`/store/product/${item.productID}`}>
+											<h3 class="text-lg font-semibold">
+												{item.products.name}
+											</h3>
+										</a>
+										<p>
+											Qty: {item.quantity}
+										</p>
+									</div>
+									<div class="ml-auto flex items-center">
+										{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
+											item.quantity * item.products.price
+										)}
+									</div>
+								</div>
+							</Table.Row>
+						{/each}
+					</Table.Body>
+				</Table.Root>
+				<div class="mt-[15px]">
 					<div class="flex">
-						<p>Shipping fee</p>
+						<p>Subtotal</p>
 						<p class="ml-auto">
 							<b>
 								{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
-									data.fee
+									data.amount
 								)}
 							</b>
 						</p>
 					</div>
-				{/if}
-				<div class="mt-[10px] flex">
-					<p class="text-lg">
-						<b>Total</b>
-					</p>
-					<p class="ml-auto text-lg">
-						<b>
-							{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
-								data.amount + data.fee
-							)}
-						</b>
-					</p>
-				</div>
-			</div>
-		</Card.Content>
-	</Card.Root>
-	{#if !data.productID}
-		<Card.Root class="w-full">
-			<Card.Header>
-				<Card.Title>Tracking</Card.Title>
-			</Card.Header>
-			<Card.Content>
-				<div>
-					{#if data.orderTracking && data.orderTracking.length > 0}
-						{#each data.orderTracking as item, i}
-							<div class="relative flex">
-								<div class="mr-4 flex flex-col items-center">
-									<div class="h-4 w-4 rounded-full bg-gray-400 dark:bg-gray-600"></div>
-									{#if i !== data.orderTracking.length - 1}
-										<div class=" absolute my-1 h-full w-[2px] bg-gray-400 dark:bg-gray-600"></div>
-									{/if}
-								</div>
-								<div class="flex-1 pb-4">
-									<p class="text-sm text-muted-foreground">
-										{new Date(item.created_at).toLocaleDateString('vi-VN')} -
-										{new Date(item.created_at).toLocaleTimeString('vi-VN')}
-									</p>
-									{#if item.link}
-										<a href={item.link}>
-											<p>{item.content}</p>
-										</a>
-									{:else}
-										<p>{item.content}</p>
-									{/if}
-								</div>
-							</div>
-						{/each}
-					{:else}
-						<p class="italic text-muted-foreground">No tracking information available yet</p>
+					{#if !data.productID}
+						<div class="flex">
+							<p>Shipping fee</p>
+							<p class="ml-auto">
+								<b>
+									{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
+										data.fee
+									)}
+								</b>
+							</p>
+						</div>
 					{/if}
+					<div class="mt-[10px] flex">
+						<p class="text-lg">
+							<b>Total</b>
+						</p>
+						<p class="ml-auto text-lg">
+							<b>
+								{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
+									data.amount + data.fee
+								)}
+							</b>
+						</p>
+					</div>
 				</div>
 			</Card.Content>
 		</Card.Root>
-	{/if}
-	<Button class="ml-auto w-fit" variant="destructive" disabled={!cancellable()} on:click={cancel}>
-		Cancel order
-	</Button>
-	<div class="mt-[25px] text-sm text-gray-400">
-		<p>Note:</p>
-		<ul>
-			<li>- Orders in delivery cannot be cancelled.</li>
-			<li>- Prepaid orders require manual cancellation.</li>
-			<li>
-				- For refunds or cancellations, please contact @nampe286 on Discord or email
-				nambuihung654@gmail.com.
-			</li>
-		</ul>
+		{#if !data.productID}
+			<Card.Root class="w-full">
+				<Card.Header>
+					<Card.Title>Tracking</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<div>
+						{#if data.orderTracking && data.orderTracking.length > 0}
+							{#each data.orderTracking as item, i}
+								<div class="relative flex">
+									<div class="mr-4 flex flex-col items-center">
+										<div class="h-4 w-4 rounded-full bg-gray-400 dark:bg-gray-600"></div>
+										{#if i !== data.orderTracking.length - 1}
+											<div class=" absolute my-1 h-full w-[2px] bg-gray-400 dark:bg-gray-600"></div>
+										{/if}
+									</div>
+									<div class="flex-1 pb-4">
+										<p class="text-sm text-muted-foreground">
+											{new Date(item.created_at).toLocaleDateString('vi-VN')} -
+											{new Date(item.created_at).toLocaleTimeString('vi-VN')}
+										</p>
+										{#if item.link}
+											<a href={item.link}>
+												<p>{item.content}</p>
+											</a>
+										{:else}
+											<p>{item.content}</p>
+										{/if}
+									</div>
+								</div>
+							{/each}
+						{:else}
+							<p class="italic text-muted-foreground">No tracking information available yet</p>
+						{/if}
+					</div>
+				</Card.Content>
+			</Card.Root>
+		{/if}
+		<Button class="ml-auto w-fit" variant="destructive" disabled={!cancellable()} on:click={cancel}>
+			Cancel order
+		</Button>
+		<div class="mt-[25px] text-sm text-gray-400">
+			<p>Note:</p>
+			<ul>
+				<li>- Orders in delivery cannot be cancelled.</li>
+				<li>- Prepaid orders require manual cancellation.</li>
+				<li>
+					- For refunds or cancellations, please contact @nampe286 on Discord or email
+					nambuihung654@gmail.com.
+				</li>
+			</ul>
+		</div>
 	</div>
-</div>
+{/if}
