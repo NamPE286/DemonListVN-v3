7faca7d7996cc1e9b203a67879dc2f6080485f5a
NamPE286 <nambuihung654@gmail.com>
2025-07-22 04:22:16 +0700

    merchant orders

--- src/routes/merchant/+page.svelte
+++ src/routes/merchant/+page.svelte
@@ -1,7 +1,19 @@
-<script lang='ts'>
-	import Title from "$lib/components/Title.svelte";
-
-    
+<script lang="ts">
+	import Title from '$lib/components/Title.svelte';
 </script>
 
 <Title value="Merchant" />
+
+<div class="wrapper">
+	<a href="/merchant/orders">Orders</a><br />
+</div>
+
+<style lang="scss">
+	a {
+		color: #7cb4f8;
+	}
+
+	.wrapper {
+		padding-inline: 75px;
+	}
+</style>
---
+++ src/routes/merchant/orders/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { user } from '$lib/client';
+	import Title from '$lib/components/Title.svelte';
+	import * as Table from '$lib/components/ui/table/index.js';
+	import { onMount } from 'svelte';
+
+	let data: any[] = [];
+
+	onMount(async () => {
+		data = await (
+			await fetch(`${import.meta.env.VITE_API_URL}/merchant/orders`, {
+				method: 'GET',
+				headers: {
+					Authorization: 'Bearer ' + (await $user.token())
+				}
+			})
+		).json();
+	});
+</script>
+
+<Title value="Orders" />
+<div class="wrapper">
+	<Table.Root>
+		<Table.Caption>Total: {data.length}</Table.Caption>
+		<Table.Header>
+			<Table.Row>
+				<Table.Head class="w-[200px]">ID</Table.Head>
+				<Table.Head class="w-[100px]">Status</Table.Head>
+				<Table.Head class="w-[150px]">Method</Table.Head>
+				<Table.Head>Recipient's name</Table.Head>
+				<Table.Head class="text-right">Amount</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each data as item}
+				<Table.Row>
+					<Table.Cell class="font-medium">
+						<a href={`/orders/${item.id}`}>
+							{item.id}
+						</a>
+					</Table.Cell>
+					<Table.Cell>{item.state}</Table.Cell>
+					<Table.Cell>{item.paymentMethod}</Table.Cell>
+					<Table.Cell>{item.recipientName}</Table.Cell>
+					<Table.Cell class="text-right">
+						{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
+							item.amount + item.fee
+						)}
+					</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		padding-inline: 50px;
+	}
+
+	@media screen and (max-width: 900px) {
+		.wrapper {
+			padding-inline: 10px;
+		}
+	}
+</style>
