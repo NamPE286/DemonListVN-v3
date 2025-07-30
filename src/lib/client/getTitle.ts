4f5df0eaa25d90e23374395ef254899d51ada847
NamPE286 <nambuihung654@gmail.com>
2025-07-31 04:13:28 +0700

    upd

--- src/lib/client/getTitle.ts
+++ src/lib/client/getTitle.ts
@@ -1,6 +1,6 @@
 export function getTitle(list: string, player: any) {
     if (list == 'dl') {
-        if (player.rating >= 5200)
+        if (player.rating >= 6000)
             return {
                 title: 'LGM',
                 fullTitle: 'Legendary Grandmaster',
@@ -12,37 +12,37 @@ export function getTitle(list: string, player: any) {
                 fullTitle: 'Grandmaster',
                 color: 'red'
             };
-        if (player.rating >= 3900)
+        if (player.rating >= 3500)
             return {
                 title: 'M',
                 fullTitle: 'Master',
                 color: 'hsla(321, 100%, 50%, 1)'
             };
-        if (player.rating >= 3500)
+        if (player.rating >= 3000)
             return {
                 title: 'CM',
                 fullTitle: 'Candidate Master',
                 color: 'purple'
             };
-        if (player.rating >= 2500)
+        if (player.rating >= 2200)
             return {
                 title: 'EX',
                 fullTitle: 'Expert',
                 color: 'blue'
             };
-        if (player.rating >= 1800)
+        if (player.rating >= 1000)
             return {
                 title: 'SP',
                 fullTitle: 'Specialist',
                 color: 'darkcyan'
             };
-        if (player.rating >= 1000)
+        if (player.rating >= 700)
             return {
                 title: 'A',
                 fullTitle: 'A',
                 color: 'green'
             };
-        if (player.rating >= 500)
+        if (player.rating >= 400)
             return {
                 title: 'B',
                 fullTitle: 'B',
