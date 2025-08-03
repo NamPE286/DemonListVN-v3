de37d3e5cd34995709a754e68d19a689d388c6cc
NamPE286 <nambuihung654@gmail.com>
2025-08-03 22:38:47 +0700

    upd

--- src/lib/client/getTitle.ts
+++ src/lib/client/getTitle.ts
@@ -1,70 +1,67 @@
 export function getTitle(list: string, player: any) {
     if (list == "dl") {
-        if (list == "dl") {
-            if (player.rating >= 4800) {
-                return {
-                    title: "LGM",
-                    fullTitle: "Legendary Grandmaster",
-                    color: "darkred",
-                };
-            }
-            if (player.rating >= 4000) {
-                return {
-                    title: "GM",
-                    fullTitle: "Grandmaster",
-                    color: "red",
-                };
-            }
-            if (player.rating >= 3000) {
-                return {
-                    title: "M",
-                    fullTitle: "Master",
-                    color: "hsla(321, 100%, 50%, 1)",
-                };
-            }
-            if (player.rating >= 2500) {
-                return {
-                    title: "CM",
-                    fullTitle: "Candidate Master",
-                    color: "purple",
-                };
-            }
-            if (player.rating >= 2000) {
-                return {
-                    title: "EX",
-                    fullTitle: "Expert",
-                    color: "blue",
-                };
-            }
-            if (player.rating >= 1500) {
-                return {
-                    title: "SP",
-                    fullTitle: "Specialist",
-                    color: "darkcyan",
-                };
-            }
-            if (player.rating >= 1000) {
-                return {
-                    title: "A",
-                    fullTitle: "A",
-                    color: "green",
-                };
-            }
-            if (player.rating >= 500) {
-                return {
-                    title: "B",
-                    fullTitle: "B",
-                    color: "#413cde",
-                };
-            }
-            if (player.rating > 0) {
-                return {
-                    title: "C",
-                    fullTitle: "C",
-                    color: "gray",
-                };
-            }
-            return null;
+        if (player.rating >= 4800) {
+            return {
+                title: "LGM",
+                fullTitle: "Legendary Grandmaster",
+                color: "darkred",
+            };
+        }
+        if (player.rating >= 4000) {
+            return {
+                title: "GM",
+                fullTitle: "Grandmaster",
+                color: "red",
+            };
+        }
+        if (player.rating >= 3000) {
+            return {
+                title: "M",
+                fullTitle: "Master",
+                color: "hsla(321, 100%, 50%, 1)",
+            };
+        }
+        if (player.rating >= 2500) {
+            return {
+                title: "CM",
+                fullTitle: "Candidate Master",
+                color: "purple",
+            };
+        }
+        if (player.rating >= 2000) {
+            return {
+                title: "EX",
+                fullTitle: "Expert",
+                color: "blue",
+            };
+        }
+        if (player.rating >= 1500) {
+            return {
+                title: "SP",
+                fullTitle: "Specialist",
+                color: "darkcyan",
+            };
+        }
+        if (player.rating >= 1000) {
+            return {
+                title: "A",
+                fullTitle: "A",
+                color: "green",
+            };
+        }
+        if (player.rating >= 500) {
+            return {
+                title: "B",
+                fullTitle: "B",
+                color: "#413cde",
+            };
+        }
+        if (player.rating > 0) {
+            return {
+                title: "C",
+                fullTitle: "C",
+                color: "gray",
+            };
         }
         return null;
     }
