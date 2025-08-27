e252d05b6d4b62bdbc688d4891af361ce6540b83
NamPE286 <nambuihung654@gmail.com>
2025-08-28 00:27:23 +0700

    elo rating dist

--- src/lib/client/getTitle.ts
+++ src/lib/client/getTitle.ts
@@ -80,76 +80,74 @@ export function getTitle(list: string, player: any) {
 			};
 		}
 
-		if (player.elo >= 6000) {
+		if (player.elo >= 3500) {
 			return {
-				title: 'AGM',
-				fullTitle: 'Ascended Grandmaster',
-				color: 'white;background: linear-gradient(to right, #ff00cc, #333399);'
-			};
-		}
-		if (player.elo >= 5000) {
-			return {
-				title: 'LGM',
-				fullTitle: 'Legendary Grandmaster',
+				title: 'Global Elite',
+				fullTitle: 'GE',
 				color: 'darkred'
 			};
 		}
-		if (player.elo >= 4000) {
+
+		if (player.elo >= 3000) {
 			return {
-				title: 'GM',
-				fullTitle: 'Grandmaster',
+				title: 'Elite',
+				fullTitle: 'E',
 				color: 'red'
 			};
 		}
-		if (player.elo >= 3000) {
+
+		if (player.elo >= 2500) {
 			return {
-				title: 'M',
-				fullTitle: 'Master',
+				title: 'Challenger First Class',
+				fullTitle: 'CFE',
 				color: 'hsla(321, 100%, 50%, 1)'
 			};
 		}
-		if (player.elo >= 2500) {
+
+		if (player.elo >= 2000) {
 			return {
-				title: 'CM',
-				fullTitle: 'Candidate Master',
+				title: 'Challenger',
+				fullTitle: 'CH',
 				color: 'purple'
 			};
 		}
-		if (player.elo >= 2000) {
-			return {
-				title: 'EX',
-				fullTitle: 'Expert',
-				color: 'blue'
-			};
-		}
+
 		if (player.elo >= 1500) {
 			return {
-				title: 'SP',
-				fullTitle: 'Specialist',
+				title: 'Diamond',
+				fullTitle: 'DM',
 				color: 'darkcyan'
 			};
 		}
+
 		if (player.elo >= 1000) {
 			return {
-				title: 'A',
-				fullTitle: 'A',
-				color: 'green'
+				title: 'Gold',
+				fullTitle: 'AU',
+				color: 'darkyellow'
 			};
 		}
+
 		if (player.elo >= 500) {
 			return {
-				title: 'B',
-				fullTitle: 'B',
-				color: '#413cde'
-			};
-		}
-		if (player.elo > 0) {
-			return {
-				title: 'C',
-				fullTitle: 'C',
+				title: 'Silver',
+				fullTitle: 'AG',
 				color: 'gray'
 			};
 		}
-		return null;
+
+		if (player.elo >= 200) {
+			return {
+				title: 'Iron',
+				fullTitle: 'FE',
+				color: 'gray'
+			};
+		}
+
+		return {
+			title: 'P',
+			fullTitle: 'Plastic',
+			color: 'P'
+		};
 	}
 }
