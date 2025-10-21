a9100bb5af1e6779b6692c8f937ccfc4effba4e9
NamPE286 <nambuihung654@gmail.com>
2025-10-22 01:55:30 +0700

    feat: empty redeem page

---
+++ src/routes/redeem/[code]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { user } from '$lib/client';
+	import { onMount } from 'svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
+	let data: any = null;
+
+	onMount(async () => {
+		data = await fetch(`${import.meta.env.VITE_API_URL}/coupon/${page.params.code}`, {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + (await $user.token())
+			}
+		});
+	});
+</script>
+
+<div class="flex items-center" style="height: calc(100vh - 150px)">
+	<Card.Root class="mx-auto w-[1000px]">
+		<Card.Content class="pt-[50px]">
+			<p>Card Content</p>
+		</Card.Content>
+	</Card.Root>
+</div>
