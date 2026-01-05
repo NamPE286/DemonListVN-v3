eae411d84f8193f5f445347c96e20fd7ac034c74
NamPE286 <nambuihung654@gmail.com>
2026-01-05 16:57:28 +0700

    fix: i18n

--- src/routes/clan/[id]/inviteButton.svelte
+++ src/routes/clan/[id]/inviteButton.svelte
@@ -33,13 +33,13 @@
 
 <Dialog.Root bind:open={opened}>
 	<Dialog.Trigger class="w-full">
-		<Button variant="outline" class="w-full">Invite</Button>
+		<Button variant="outline" class="w-full">{$_('clan.invite.button')}</Button>
 	</Dialog.Trigger>
 	<Dialog.Content>
 		<Dialog.Header>
-			<Dialog.Title>{$_('invite.title')}</Dialog.Title>
+			<Dialog.Title>{$_('clan.invite.title')}</Dialog.Title>
 		</Dialog.Header>
-		<Input placeholder={$_('invite.placeholder')} bind:value={invitePlayerUID} />
-		<Button on:click={invitePlayer} disabled={invitePlayerUID.length == 0}>{$_('invite.button')}</Button>
+			<Input placeholder={$_('clan.invite.placeholder')} bind:value={invitePlayerUID} />
+			<Button on:click={invitePlayer} disabled={invitePlayerUID.length == 0}>{$_('clan.invite.button')}</Button>
 	</Dialog.Content>
 </Dialog.Root>
