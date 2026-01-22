82ba1fa07aef00685cbc37ffd528d28a198b0f90
NamPE286 <nambuihung654@gmail.com>
2026-01-22 12:01:16 +0700

    refactor

--- src/lib/components/RewardButton.svelte
+++ src/lib/components/RewardButton.svelte
@@ -2,6 +2,7 @@
 	import Gift from 'lucide-svelte/icons/gift';
 	import Lock from 'lucide-svelte/icons/lock';
 	import * as Popover from '$lib/components/ui/popover/index.js';
+	import RewardItemDetails from '$lib/components/RewardItemDetails.svelte';
 	import { get } from 'svelte/store';
 	import { _ } from 'svelte-i18n';
 
@@ -17,36 +18,6 @@
 	$: isPremiumLocked = isPremiumTrack && !isPremiumActivated;
 	// Determine if item should have reduced opacity (tier not reached but accessible)
 	$: shouldReduceOpacity = !isClaimable && !isPremiumLocked;
-
-	function rarityColor(r: number) {
-		switch (r) {
-			case 1:
-				return '#3b82f6';
-			case 2:
-				return '#a855f7';
-			case 3:
-				return '#ec4899';
-			case 4:
-				return '#dc2626';
-			default:
-				return '#9ca3af';
-		}
-	}
-
-	function rarityName(r: number) {
-		switch (r) {
-			case 1:
-				return get(_)('inventory.rarity_names.uncommon');
-			case 2:
-				return get(_)('inventory.rarity_names.rare');
-			case 3:
-				return get(_)('inventory.rarity_names.epic');
-			case 4:
-				return get(_)('inventory.rarity_names.legendary');
-			default:
-				return get(_)('inventory.rarity_names.common');
-		}
-	}
 
 	$: itemData = reward?.items || reward;
 </script>
@@ -110,36 +81,7 @@
 		</Popover.Trigger>
 		{#if itemData}
 			<Popover.Content>
-				<div class="flex items-start gap-3">
-					<img
-						class="h-16 w-16 object-contain"
-						src={`https://cdn.demonlistvn.com/items/${itemData.id || reward.itemId}.webp`}
-						alt={itemData.name}
-					/>
-					<div>
-						<div class="font-medium">{itemData.name}</div>
-						{#if itemData.rarity !== undefined}
-							<div class="text-sm text-neutral-400">
-								<b>{get(_)('events.quest.rarity_label')}</b>
-								<span class="font-bold" style="color: {rarityColor(itemData.rarity)}"
-									>{rarityName(itemData.rarity)}</span
-								>
-							</div>
-						{/if}
-						{#if itemData.description}
-							<div class="text-sm text-neutral-400">
-								<b>{get(_)('events.quest.description_label')}:</b>
-								{itemData.description}
-							</div>
-						{/if}
-						{#if reward.quantity > 1}
-							<div class="text-sm text-neutral-400">
-								<b>{get(_)('events.quest.quantity_label')}:</b>
-								{reward.quantity}
-							</div>
-						{/if}
-					</div>
-				</div>
+				<RewardItemDetails itemData={itemData} quantity={reward.quantity} />
 			</Popover.Content>
 		{/if}
 	</Popover.Root>
